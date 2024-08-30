ffac60329b3353867835d9019c9e8dffaa899f8f
LiorTabib99 <lior7999@gmail.com>
2024-08-30 13:45:56 +0300

    fixing mix letters compoent and deleting end game mixed letters compoenent with is not in use

--- src/app/mix-letters/mix-letters.component.ts
+++ src/app/mix-letters/mix-letters.component.ts
@@ -1,71 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { CategoriesService } from '../services/categories.service';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { FormsModule } from '@angular/forms';
-import { TranslatedWord } from '../../shared/model/translatedWord';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriesService } from '../services/categories.service';
+import { MatDialog} from '@angular/material/dialog';
 import { pointsScoreService } from '../services/pointsScore.service';
 import { CorrectAnswersComponent } from '../correct-answers/correct-answers.component';
-// import { IcorrectAnswersComponent } from '../icorrect-answers/icorrect-answers.component';
+import { IcorrectAnswersComponent } from '../icorrect-answers/icorrect-answers.component';
 import { ExitGameDialogComponent } from '../exit-game-dialog/exit-game-dialog.component';
+import { TranslatedWord } from '../../shared/model/translatedWord';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { gameResultData } from '../../shared/model/gameResultData';
 import { GamesResultService } from '../services/gameResults.service';
-
 @Component({
-  selector: 'app-mix-letters',
-  standalone: true,
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressBarModule,
-    FormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-  ],
+  selector: 'app-mixed-letters-game',
   templateUrl: './mix-letters.component.html',
   styleUrls: ['./mix-letters.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatIconModule,
+  ],
 })
+
 export class MixLettersComponent implements OnInit {
-  words: TranslatedWord[] = [];
+  words: TranslatedWord[] = []; // Array to store TranslatedWord objects
   currentWordIndex: number = 0;
-  currentWord: TranslatedWord = {
-    origin: '',
-    target: '',
-    guess: '',
-  };
-  mixedWord: string = '';
+  currentWord: TranslatedWord = { origin: '', target: '', guess: '' }; // TranslatedWord object for the current word
+  mixedword: string = '';
   guessInput: string = '';
   message: string = '';
-  attempts: number = 0;
   score: number = 0;
-  grade: number = 0;
+  grade: number = 100;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   answers: any[] = [];
   categoryName: string = '';
-  totalQuestions: number = 0;
-  currentQuestion: number = 0;
-
+  totalQuestions: number = 0; // Total number of questions
+  currentQuestion: number = 0; // Index of the current question
   attemptsLeft: { [key: string]: number } = {}; // Tracks attempts left for each word
-
-  // Tracks words that have been answered
-  answeredWords: Set<string> = new Set();
-  guess: string = '';
-
+  answeredWords: Set<string> = new Set(); // Tracks words that have been answered
+  
   constructor(
     private route: ActivatedRoute,
-    private categoryService: CategoriesService,
-    private scorceService: pointsScoreService,
-    private router: Router,
+    private categoriesService: CategoriesService,
     private dialog: MatDialog,
-    private gamesResultService: GamesResultService
+    private scoreService:pointsScoreService,
+    private router: Router,
+    private gameResultService: GamesResultService
   ) {}
 
   ngOnInit(): void {
@@ -73,6 +63,7 @@ export class MixLettersComponent implements OnInit {
       const categoryId = +params['categoryId'];
       this.loadWordsFromCategory(categoryId);
     });
+    this.score = this.scoreService.getCurrentScore();
   }
 
   loadWordsFromCategory(categoryId: number): void {
@@ -80,61 +71,58 @@ export class MixLettersComponent implements OnInit {
       if (categoryId === 0) {
         this.handleSpecialCategory();
       } else {
-        const cateogry = this.categoryService.get(categoryId);
-        if (cateogry) {
-          this.words = cateogry.words;
-          this.categoryName = cateogry.name;
-          console.log(this.categoryName);
-          this.totalQuestions = this.words.length;
-          this.initializeAttempts();
+        const category = this.categoriesService.get(categoryId);
+        if (category) {
+          this.words = category.words; // Load TranslatedWord objects directly
+          this.categoryName = category.name;
+          this.totalQuestions = this.words.length; // Set the total number of questions
+          this.initializeAttempts(); // Initialize attempts for each word
           this.newGame();
         } else {
-          this.message = 'category not found';
+          this.message = 'Category not found!';
         }
       }
     } else {
-      this.message = 'Invalid cateogryId';
+      this.message = 'Invalid category ID!';
     }
   }
 
-  //handle the first cateogry and start the game to a new game
   handleSpecialCategory(): void {
-    const defualtCateogry = this.categoryService.get(0);
-    if (defualtCateogry) {
-      this.categoryName = defualtCateogry.name;
-      this.words = defualtCateogry.words;
-      this.totalQuestions = this.words.length;
-      this.initializeAttempts();
+    const defaultCategory = this.categoriesService.get(0);
+    if (defaultCategory) {
+      this.words = defaultCategory.words;
+      this.totalQuestions = this.words.length; // Set the total number of questions
+      this.initializeAttempts(); // Initialize attempts for each word
       this.newGame();
     } else {
-      this.message = "Default cateogry wan't found";
+      this.message = 'Default category not found!';
     }
   }
 
   initializeAttempts(): void {
-    this.words.forEach((word) => {
-      this.attemptsLeft[word.origin] = 3;
+    this.words.forEach(word => {
+      this.attemptsLeft[word.origin] = 3; // Set 3 attempts for each word
     });
   }
 
-  //Will create a new game
   newGame(): void {
-    this.attempts = 0;
-    this.currentQuestion = 0;
+    this.currentQuestion = 0; // Reset the current question index
     this.currentWordIndex = 0;
+    this.grade = 100;
+    this.answers = [];
+    this.answeredWords.clear();
+    this.message = '';
     this.loadWord();
   }
 
   loadWord(): void {
-    this.currentWord = this.words[this.currentWordIndex];
-    this.mixedWord = this.scrambelredWord(
-      this.currentWord.origin.split('')
-    ).join(' ');
+    this.currentWord = this.words[this.currentWordIndex]; // Set the current TranslatedWord object
+    this.mixedword = this.shuffle(this.currentWord.origin.split('')).join(' '); // Shuffle the origin word
     this.guessInput = '';
     this.message = '';
   }
 
-  scrambelredWord(array: string[]): string[] {
+  shuffle(array: string[]): string[] {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -143,31 +131,29 @@ export class MixLettersComponent implements OnInit {
   }
 
   checkGuess(): void {
-    const isCorrect =
-      this.guess.toLowerCase() === this.currentWord.origin.toLowerCase();
+    const isCorrect = this.guessInput.toLowerCase() === this.currentWord.origin.toLowerCase();
 
     if (!this.answeredWords.has(this.currentWord.origin)) {
       // Check if this word has already been answered
       if (isCorrect) {
-        this.currentQuestion++;
+        this.currentQuestion++
         this.answeredWords.add(this.currentWord.origin); // Mark word as answered
         this.message = 'Correct! 🎉';
         this.dialog.open(CorrectAnswersComponent, {
           data: {
             message: 'Correct! 🎉',
             origin: this.currentWord.origin,
-            target: this.currentWord.target,
+            target: this.currentWord.target
           },
         });
         this.score++;
-        this.scorceService.updateScore(this.score);
-        this.answers.push({
-          // Push answer
-          question: this.mixedWord.replace(/ /g, ''),
+        this.scoreService.updateScore(this.score);
+        this.answers.push({ // Push answer
+          question: this.mixedword.replace(/ /g, ''),
           answer: this.currentWord.origin,
           isCorrect: true,
           origin: this.currentWord.origin,
-          target: this.currentWord.target,
+          target: this.currentWord.target
         });
         this.nextWord();
       } else {
@@ -177,25 +163,23 @@ export class MixLettersComponent implements OnInit {
 
         if (this.attemptsLeft[this.currentWord.origin] <= 0) {
           this.answeredWords.add(this.currentWord.origin); // Mark word as answered after all attempts
-          this.answers.push({
-            // Push answer after all attempts
-            question: this.mixedWord.replace(/ /g, ''),
+          this.answers.push({ // Push answer after all attempts
+            question: this.mixedword.replace(/ /g, ''),
             answer: this.currentWord.origin,
             isCorrect: false,
             origin: this.currentWord.origin,
-            target: this.currentWord.target,
+            target: this.currentWord.target
           });
-          this.currentQuestion++;
+          this.currentQuestion++
         }
 
-        this.dialog.open, {
+        this.dialog.open(IcorrectAnswersComponent, {
           data: {
             message: 'Incorrect! Try Again!',
             origin: this.currentWord.origin,
-            target: this.currentWord.target,
-            width: ""
+            target: this.currentWord.target
           },
-        };
+        });
 
         if (this.grade <= 0) {
           this.grade = 0;
@@ -209,26 +193,6 @@ export class MixLettersComponent implements OnInit {
     }
   }
 
-  endGame(): void {
-    this.scorceService.addedGamePlayed('Mixed Letters', this.score);
-    this.message = `You translared ${this.currentQuestion} out of ${this.totalQuestions} words correctly`;
-    const gameResultsData: gameResultData = {
-      message: this.message,
-      answers: this.answers,
-      grade: this.grade,
-      score: this.score,
-      categoryName: this.categoryName,
-    };
-    this.gamesResultService.setResultData(gameResultsData);
-    this.router.navigate(['/mix-letters-results']);
-  }
-
-  //Will reset thee game to a new game
-  resetGame(): void {
-    this.newGame();
-  }
-
-  //checks if the lengeth of the words on the array
   nextWord(): void {
     this.currentWordIndex++;
     if (this.currentWordIndex < this.words.length) {
@@ -238,9 +202,27 @@ export class MixLettersComponent implements OnInit {
     }
   }
 
-  //exiting the game
+  resetGame(): void {
+    this.newGame();
+  }
+
+  endGame(): void {
+    this.scoreService.addedGamePlayed('Mixed Letters', this.score);
+    this.message = `You translared ${this.currentQuestion} out of ${this.totalQuestions} words correctly`;
+    const gameResultData: gameResultData = {
+      message: this.message,
+      answers: this.answers,
+      grade: this.grade,
+      score: this.score,
+      categoryName: this.categoryName
+    };
+    this.gameResultService.setResultData(gameResultData);
+    this.router.navigate(['/mix-letters-results']);
+  }
+
   openExitDialog(): void {
     const dialogRef = this.dialog.open(ExitGameDialogComponent);
+
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.router.navigate(['/main']);
@@ -248,12 +230,16 @@ export class MixLettersComponent implements OnInit {
     });
   }
 
-  //returning true if it is hebrew word
   isHebrewWord(word: string): boolean {
     return /[\u0590-\u05FF]/.test(word);
   }
 
   get proccess(): number {
-    return (this.currentQuestion / this.totalQuestions) * 100;
-  }
+    return (this.currentQuestion / this.totalQuestions) * 100;
 }
+
+}
+
+
+
+
